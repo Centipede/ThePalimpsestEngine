031eda9be1661de57eae7088d83ca3c6a95cb4a3
Rene Jensen <rene@catatonic.dk>
2026-08-17 18:32:16 +0200

    Collapsed paragraph rows show only long summary.

--- Apps/Palimpsest-Web/src/components/ContentBlockView.vue
+++ Apps/Palimpsest-Web/src/components/ContentBlockView.vue
@@ -1,5 +1,5 @@
 <template>
-  <div :id="'block-' + block.path_id" class="content-block">
+  <div :id="'block-' + block.path_id" class="content-block" :class="{ 'is-folded': isFolded }">
     <!-- Leftmost: Toggle -->
     <div class="block-toggle">
       <sl-icon-button
@@ -12,26 +12,34 @@
 
     <!-- Left outer: Block link -->
     <div class="block-info">
-      <div class="block-link">
-        Paragraph {{ index + 1 }}<br>
-        {{ block.path_id }}
+      <div class="block-link" title="{{ block.path_id }}">
+        Paragraph {{ index + 1 }}
       </div>
     </div>
 
-    <!-- Left inner: Summary (caption) -->
-    <div class="block-summary">
-      <small v-if="summaryCaption"><i>{{ summaryCaption }}</i></small>
-    </div>
+    <template v-if="isFolded">
+      <!-- Center: Wide Summary (folded) -->
+      <div class="block-summary">
+        <small v-if="summaryText"><i>{{ summaryText }}</i></small>
+      </div>
+    </template>
 
-    <!-- Center: Content -->
-    <div v-show="!isFolded" class="block-content">
-      <div class="content-text">{{ block.content_text }}</div>
-    </div>
+    <template v-else>
+      <!-- Left inner: Summary (caption) -->
+      <div class="block-summary">
+        <small v-if="summaryCaption"><i>{{ summaryCaption }}</i></small>
+      </div>
 
-    <!-- Right inner: Entities -->
-    <div v-show="!isFolded" class="block-entities">
-      <small v-if="entitiesFuzzy"><i>{{ entitiesFuzzy }}</i></small>
-    </div>
+      <!-- Center: Content -->
+      <div class="block-content">
+        <div class="content-text">{{ block.content_text }}</div>
+      </div>
+
+      <!-- Right inner: Entities -->
+      <div class="block-entities">
+        <small v-if="entitiesFuzzy"><i>{{ entitiesFuzzy }}</i></small>
+      </div>
+    </template>
 
     <!-- Right outer: Page link -->
     <div class="block-pages">
@@ -64,6 +72,11 @@ const summaryCaption = computed(() => {
   return (record?.content_js as SummaryInfoContent)?.caption;
 });
 
+const summaryText = computed(() => {
+  const record = props.block.inforecords.find(r => r.kind === 'summary');
+  return (record?.content_js as SummaryInfoContent)?.summary;
+});
+
 const entitiesFuzzy = computed(() => {
   const record = props.block.inforecords.find(r => r.kind === 'named_entities');
   return (record?.content_js as EntitiesInfoContent)?.entities?.fuzzy_answer;
@@ -82,6 +95,7 @@ const lastPage = computed(() => {
 .content-block {
   display: grid;
   grid-template-columns: 30px 100px 1fr 4fr 1fr 70px 30px;
+  grid-template-areas: "toggle info summary content entities pages spacer";
   grid-gap: 1.5rem;
   align-items: first baseline;
   width: 100%;
@@ -89,17 +103,24 @@ const lastPage = computed(() => {
   border-bottom: 1px solid var(--color-border, #e5e7eb);
 }
 
+.content-block.is-folded {
+  grid-template-columns: 30px 100px 1fr 70px 30px;
+  grid-template-areas: "toggle info summary pages spacer";
+}
+
 .content-block:last-child {
   border-bottom: none;
 }
 
 .block-toggle {
+  grid-area: toggle;
   display: flex;
   align-items: center;
   justify-content: center;
 }
 
 .block-info {
+  grid-area: info;
   font-size: 0.75rem;
   line-height: 1.3;
 }
@@ -110,7 +131,8 @@ const lastPage = computed(() => {
   text-decoration: none;
 }
 
-.block-summary, .block-entities {
+.block-summary {
+  grid-area: summary;
   color: var(--sl-color-neutral-600);
   font-size: 0.875rem;
   line-height: 1.4;
@@ -118,10 +140,16 @@ const lastPage = computed(() => {
 }
 
 .block-entities {
+  grid-area: entities;
+  color: var(--sl-color-neutral-600);
+  font-size: 0.875rem;
+  line-height: 1.4;
+  word-break: break-word;
   white-space: pre-wrap;
 }
 
 .block-content {
+  grid-area: content;
   width: 100%;
 }
 
@@ -134,7 +162,7 @@ const lastPage = computed(() => {
 }
 
 .block-pages {
-  grid-column: 6;
+  grid-area: pages;
   font-size: 0.75rem;
   color: var(--sl-color-neutral-500);
   text-align: right;
@@ -142,7 +170,7 @@ const lastPage = computed(() => {
 }
 
 .block-spacer {
-  grid-column: 7;
+  grid-area: spacer;
 }
 
 @media (max-width: 1024px) {
@@ -151,6 +179,11 @@ const lastPage = computed(() => {
     grid-template-areas: 
       "toggle info content content pages spacer"
       ". . summary entities . .";
+  }
+
+  .content-block.is-folded {
+    grid-template-columns: 30px 80px 1fr 80px 30px;
+    grid-template-areas: "toggle info summary pages spacer";
   }
 }
 </style>
